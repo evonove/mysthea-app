6200bde737372a5d7106518bd0c1c22482e5a4f5
Federico Dolce <psykopear@gmail.com>
2020-04-27 15:44:02 +0200

    Improved generate.py to generate also .ts file

--- tools/translations/generate.py
+++ tools/translations/generate.py
@@ -1,26 +1,66 @@
+import xml.etree.ElementTree as ET
+import xml.dom.minidom
 import argparse
 import csv
 
-def generate(original_file_path, fields, delimiter):
+def generate(original_file_path, lang, prefix):
     with open(original_file_path, 'r') as file:
-        data = list(csv.DictReader(file, delimiter=delimiter))
+        data = list(csv.DictReader(file, delimiter=','))
 
-    processed = []
+    # First rearrange data in a dict compose like thid:
+    # { "<code>": { "<field>": { "orig": "<original text>", "<lang>": "<translated text>" } } }
+    processed = {}
+    trans_fields = ["TYPE", "COMMAND", "NAME", "CONTENT"]
     for element in data:
-        for field in fields:
-            value = element[field]
-            if value and value not in processed:
-                processed.append(value)
+        code = element["CODE"]
+        processed[code] = {}
+        for field in trans_fields:
+            if element[field]:
+                processed[code][field] = {}
+                processed[code][field]["orig"] = element[field]
+                processed[code][field][lang] = element[f"{field}_{lang}"]
 
-    with open("result.js", "w") as out:
-        for line in processed:
-            out.write(f"QT_TRANSLATE_NOOP(\"CardsData\", \"{repr(line)[1:-1]}\")\n")
+    # Generate the XML tree for the translations file
+    ts = ET.Element("TS", version="2.1", language=f"{lang.lower()}_{lang}")
+    context = ET.SubElement(ts, "context")
+    name = ET.SubElement(context, "name")
+    name.text = "CardsData"
+
+    # This is only used to avoid processing two times the same line
+    processed_lines = []
+
+    # Output js filename
+    out_js_filename = f"{prefix}_{lang}.js"
+    with open(out_js_filename, "w") as js_out:
+        count = 1
+        for code, data in processed.items():
+            for field in data.values():
+                # Extract original text with all '\n' characters included
+                source_text = repr(field['orig'])[1:-1]
+                if source_text not in processed_lines:
+                    processed_lines.append(source_text)
+                    # Write the line to the js output file first
+                    js_out.write(f"QT_TRANSLATE_NOOP(\"CardsData\", \"{source_text}\")\n")
+                    # Create the XML element for translations data
+                    msg = ET.SubElement(context, "message")
+                    ET.SubElement(msg, "location", filename=out_js_filename, line=f"{count}")
+                    source = ET.SubElement(msg, "source")
+                    source.text = source_text
+                    # Set translation as unfinished, it will have to be checked manually
+                    trans = ET.SubElement(msg, "translation", type="unfinished")
+                    # Write the actual translation text
+                    trans.text = field[lang]
+                    # Keep js file's line count updated
+                    count += 1
+        # Write the final .ts file, to be opened in QT linguist
+        tree = ET.ElementTree(ts)
+        tree.write(f"{prefix}_{lang}.ts", encoding="utf-8", xml_declaration=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert a CSV file into a js file usable by QT')
-    parser.add_argument('-d', '--delimiter', default=',', help='Delimiter used in the CSV file, defaults to ","')
     parser.add_argument('original_file', help='path of the CSV file to process')
-    parser.add_argument('fields', metavar='field', type=str, nargs='+',help='Fields to extract from the CSV')
+    parser.add_argument('prefix', help='Name of the application to translate (icaion, mysthea, thefall)')
+    parser.add_argument('lang', help='Lang code (eg IT, ES, JP) output')
     args = parser.parse_args()
-    generate(args.original_file, args.fields, args.delimiter)
+    generate(args.original_file, args.lang, args.prefix)
